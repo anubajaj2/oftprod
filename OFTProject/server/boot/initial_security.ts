d92149224b9fd0dfedef1f423954330f3a42eb3f
anubajaj2 <anurag.bajaj02@gmail.com>
2020-05-26 09:46:06 +0530

    security file

--- OFTProject/server/boot/initial_security.ts
+++ OFTProject/server/boot/initial_security.ts
@@ -1,121 +1,102 @@
-declare var module: any;
+"use strict";
+var log4js = require('log4js');
+var logger = log4js.getLogger("odata");
+var InitSecurity = (function () {
+    function InitSecurity(app) {
+        this.User = app.models.User;
+        this.Role = app.models.Role;
+        this.AppUser = app.models.AppUser;
+        this.RoleMapping = app.models.RoleMapping;
+    } ;
+    InitSecurity.prototype.init = function () {
+        var _this = this;
+        this.User.findOne({where: {email: 'kajoljain@evotrainingsolutions\.com'}}).then((user) => {
+    			if(!user) {
+    				// create user if not already exists
+    				this.User.create({username: 'kajol', email: 'kajoljain@evotrainingsolutions\.com', password: 'Kajol@123'}).then((user) => {
+    					if(user) {
+    						logger.debug(`User created: ${JSON.stringify(user.toJSON())}`);
+    						this.initRoleForUser(user);
+    					} else {
+    						logger.error(`user 'kajol' could not be created. Program may not work as expected`);
+    					}
+    				});
+    			} else {
+    				this.initRoleForUser(user);
+    			}
+    		}).catch((err) => {
+    			logger.error(`error: ${err}`);
+    		});
+    };
+    InitSecurity.prototype.initRoleForUser = function (user) {
+        var _this = this;
+        this.Role.findOne({ where: { name: 'r_admin' }, include: 'principals' }).then(function (role) {
+            if (!role) {
+                _this.Role.create({ name: 'r_admin', description: 'grants general access to businesstrips' }).then(function (role) {
+                    if (role) {
+                        //logger.debug("Role created: " + JSON.stringify(role.toJSON()));
+                        _this.assignUserToRole(user, role);
+                    }
+                    else {
+                      //  logger.error("role 'r_admin' could not be created. Program may not work as expected");
+                    }
+                });
+            }
+            else {
+            //  _this.assignUserToRole(user, role);
+            }
+        });
 
-import log4js = require('log4js');
-let logger = log4js.getLogger("odata");
+    };
+    InitSecurity.prototype.initClearkRoleForUser = function (user) {
+        var _this = this;
+        this.AppUser.findOne({ where: { "EmailId": user.email } }).then(function (roleMapping) {
+            if (!roleMapping) {
+                _this.AppUser.create({
+                    TechnicalId: user.id,
+                    EmailId: user.email,
+                    UserName: user.username,
+                    Role:"Content"
+                }).then(function (roleMapping) {
+                    //logger.debug("Rolemapping created: " + JSON.stringify(roleMapping.toJSON()));
+                });
+            }
+        });
 
-
-class InitSecurity {
-	private User;
-	private Role;
-	private RoleMapping;
-
-	constructor(app) {
-		this.User = app.models.User;
-		this.Role = app.models.Role;
-		this.RoleMapping = app.models.RoleMapping;
-	};
-
-	public init() {
-		// find user1 in db
-		this.User.findOne({where: {email: 'anubhav\.abap@gmail\.com'}}).then((user) => {
-			if(!user) {
-				// create user if not already exists
-				this.User.create({username: 'anubhav', email: 'anubhav\.abap@gmail\.com', password: 'Anurag420'}).then((user) => {
-					if(user) {
-						logger.debug(`User created: ${JSON.stringify(user.toJSON())}`);
-						this.initRoleForUser(user);
-					} else {
-						logger.error(`user 'anubhav' could not be created. Program may not work as expected`);
-					}
-				});
-			} else {
-				this.initRoleForUser(user);
-			}
-		}).catch((err) => {
-			logger.error(`error: ${err}`);
-		});
-
-		this.User.findOne({where: {email: 'install\.abap@gmail\.com'}}).then((user) => {
-			if(!user) {
-				// create user if not already exists
-				this.User.create({username: 'minakshi', email: 'anubhav\.abap@gmail\.com', password: 'Minakshi@123'}).then((user) => {
-					if(user) {
-						logger.debug(`User created: ${JSON.stringify(user.toJSON())}`);
-						this.initRoleForUser(user);
-					} else {
-						logger.error(`user 'minakshi' could not be created. Program may not work as expected`);
-					}
-				});
-			} else {
-				this.initRoleForUser(user);
-			}
-		}).catch((err) => {
-			logger.error(`error: ${err}`);
-		});
-	};
-
-	/**
-	 * creates the role r_admin that grants access to the businesstrips model and assign it to
-	 * the passed user
-	 * @param user user to assign the role to
-	 */
-	private initRoleForUser(user:any) {
-		this.Role.findOne({where: {name: 'r_admin'}, include: 'principals'}).then((role) => {
-			if(!role) {
-				this.Role.create({name: 'r_admin', description: 'grants general access to Admin functions'}).then((role) => {
-					if(role) {
-						logger.debug(`Role created: ${JSON.stringify(role.toJSON())}`);
-						this.assignUserToRole('anubhav', role);
-					} else {
-						logger.error(`role 'r_admin' could not be created. Program may not work as expected`);
-					}
-				})
-			} else {
-				this.assignUserToRole(user, role);
-			}
-		});
-
-		this.Role.findOne({where: {name: 'r_clerk'}, include: 'principals'}).then((role) => {
-			if(!role) {
-				this.Role.create({name: 'r_clerk', description: 'grants general access to Cleark'}).then((role) => {
-					if(role) {
-						logger.debug(`Role created: ${JSON.stringify(role.toJSON())}`);
-						this.assignUserToRole('minakshi', role);
-					} else {
-						logger.error(`role 'r_clerk' could not be created. Program may not work as expected`);
-					}
-				})
-			} else {
-				this.assignUserToRole(user, role);
-			}
-		});
-
-	}
-
-	/**
-	 * assigns the passed user to the passed role if not already done
-	 * @param user user to assign to the role
-	 * @param role role the user to assign to
-	 */
-	private assignUserToRole(user, role) {
-		// Promise (then) does not work here
-		this.RoleMapping.findOne({where: {principalType: this.RoleMapping.USER, principalId: user.id, roleId: role.id}}).then((roleMapping) => {
-			if(!roleMapping) {
-				role.principals.create({
-					principalType: this.RoleMapping.USER,
-					principalId: user.id
-				}).then((roleMapping) => {
-					logger.debug(`Rolemapping created: ${JSON.stringify(roleMapping.toJSON())}`);
-				})
-			}
-		})
-	}
-
-
-}
-
+    };
+    InitSecurity.prototype.assignAdminUserToRole = function (user) {
+        var _this = this;
+        this.AppUser.findOne({ where: { "EmailId": user.email } }).then(function (roleMapping) {
+            if (!roleMapping) {
+                _this.AppUser.create({
+                    TechnicalId: user.id,
+                    EmailId: user.email,
+                    UserName: user.username,
+                    Role:"Admin"
+                }).then(function (roleMapping) {
+                  //  logger.debug("Rolemapping created: " + JSON.stringify(roleMapping.toJSON()));
+                });
+            }
+        });
+    };
+    InitSecurity.prototype.assignUserToRole = function (user, role) {
+        var _this = this;
+        this.RoleMapping.findOne({ where: { principalType: this.RoleMapping.USER, principalId: user.id, roleId: role.id } }).then(function (roleMapping) {
+            if (!roleMapping) {
+                role.principals.create({
+                    principalType: _this.RoleMapping.USER,
+                    principalId: user.id
+                }).then(function (roleMapping) {
+                  //  logger.debug("Rolemapping created: " + JSON.stringify(roleMapping.toJSON()));
+                });
+            }
+        });
+    };
+    return InitSecurity;
+}());
 module.exports = function initial_security(app) {
-	logger.debug(`starting initial_security script`);
-	let initSecurity:InitSecurity = new InitSecurity(app);
-	initSecurity.init();
+    logger.debug("starting initial_security script");
+    var initSecurity = new InitSecurity(app);
+    initSecurity.init();
 };
+//# sourceMappingURL=initial_security.js.map
