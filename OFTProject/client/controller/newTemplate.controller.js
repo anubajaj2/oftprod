24014865082bcab62e782706499927be72bf2949
yogendra-code <yogendra@evotrainingsolutions.com>
2020-11-20 18:02:49 +0530

    template

--- OFTProject/client/controller/newTemplate.controller.js
+++ OFTProject/client/controller/newTemplate.controller.js
@@ -24,47 +24,45 @@ sap.ui.define([
 				this.getView().byId("idUser").setText(loginUser);
 			}
 		},
-		handleUploadComplete: function(oEvent) {
-			var sResponse = oEvent.getParameter("response");
-			if (sResponse) {
-				var sMsg = "";
-				debugger;
-				var m = /^\[(\d\d\d)\]:(.*)$/.exec(sResponse);
-				if (m[1] == "200") {
-					$.post('/upload', {
-						files: oEvent.getSource().getFocusDomRef().files[0]
-					})
-						.done(function(data, status){
-									sap.m.MessageBox.error("Done");
-						})
-						.fail(function(xhr, status, error) {
-									sap.m.MessageBox.error("Error in upload");
-						});
-					sMsg = "Return Code: " + m[1] + "\n" + m[2] + "(Upload Success)";
-					oEvent.getSource().setValue("");
-				} else {
-					sMsg = "Return Code: " + m[1] + "\n" + m[2] + "(Upload Error)";
-				}
 
-				MessageToast.show(sMsg);
+		herculis: function(oEvent) {
+			var that = this;
+			if(oEvent.getParameter("name") !== "newTemplate"){
+				return;
 			}
+
+			var oList = this.getView().byId("idRecent");
+			oList.bindAggregation("items", {
+				path: '/Templates',
+				template: new sap.m.DisplayListItem({
+					label: "{CourseName} - {Type} - {DemoDate}",
+					value: "{ClassTiming} / {NextClass}"
+				})
+			});
+			var type = this.getView().byId("type").getSelectedKey();
+			var courseName = this.getView().byId("course").getSelectedKey();
+			var oFilter1 = new sap.ui.model.Filter("CourseName", "EQ", courseName);
+			var oFilter2 = new sap.ui.model.Filter("Type", "EQ", type);
+			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "GET", {
+					filters: [oFilter1,oFilter2]
+				}, {}, this)
+				.then(function(oData){
+					if(oData.results.length<1){
+						that.getView().byId('idSave').setEnabled(true);
+						that.getView().byId('idUpdate').setEnabled(false);
+					}
+					else{
+						that.getView().byId('idSave').setEnabled(false);
+						that.getView().byId('idUpdate').setEnabled(true);
+					}
+					that.getView().getModel('local').setProperty('/template',oData.results[0]);
+				}).catch(function(oError){
+					sap.m.MessageToast.show("template fetch failed");
+				});
+			// oList.attachUpdateFinished(this.counter);
+
 		},
 
-		handleUploadPress: function(oEvent) {
-			var oFileUploader = this.byId("fileUploader");
-			oFileUploader.upload();
-		},
-		updateInq: function(){
-				var that = this;
-				//gtest
-				$.post('/upload')
-			    .done(function(data, status){
-								sap.m.MessageBox.error("Done");
-					})
-			    .fail(function(xhr, status, error) {
-								sap.m.MessageBox.error("Error in upload");
-			    });
-		},
 		clearForm: function() {
 			// this.getView().getModel("local").setProperty("/template",{
 			// 	"emailId": "",
@@ -78,248 +76,135 @@ sap.ui.define([
 			// 	"message": ""
 			// });
 		},
-		passwords: "",
-		onEmail: function() {
-			var that = this;
-			var items = that.getView().byId('idRecent').getSelectedContexts();
-
-			for (var i = 0; i < items["length"]; i++) {
-				var loginPayload = items[i].getModel().getProperty(items[i].getPath());
-				if (this.passwords === "") {
-					this.passwords = prompt("Please enter your password", "");
-					if (this.passwords === "") {
-						sap.m.MessageBox.error("Blank Password not allowed");
-						return;
-					}
-				}
-				loginPayload.password = this.passwords;
-				loginPayload.DollerQuote = this.getView().byId("doller").getSelected();
-				$.post('/sendInquiryEmail', loginPayload)
-					.done(function(data, status) {
-						sap.m.MessageToast.show("Email sent successfully");
-					})
-					.fail(function(xhr, status, error) {
-						that.passwords = "";
-						sap.m.MessageBox.error(xhr.responseText);
-					});
-			}
-		},
-		onDataExport: function(oEvent) {
-			$.ajax({
-				type: 'GET', // added,
-				url: 'InquiryDownload',
-				success: function(data) {
-					sap.m.MessageToast.show("File Downloaded succesfully");
-				},
-				error: function(xhr, status, error) {
-					sap.m.MessageToast.show("error in downloading the excel file");
-				}
-			});
-
-		},
-		onDelete: function(oEvent) {
-			var that = this;
-			MessageBox.confirm("Do you want to delete the selected records?", function(conf) {
-				if (conf == 'OK') {
-					var items = that.getView().byId('idRecent').getSelectedContexts();
-					for (var i = 0; i < items["length"]; i++) {
-						that.ODataHelper.callOData(that.getOwnerComponent().getModel(), items[i].sPath,
-								"DELETE", {}, {}, that)
-							.then(function(oData) {
-								sap.m.MessageToast.show("Deleted succesfully");
-							}).catch(function(oError) {
-								that.getView().setBusy(false);
-								that.oPopover = that.getErrorMessage(oError);
-								that.getView().setBusy(false);
-							});
-
-					}
-				}
-			}, "Confirmation");
-
-
-		},
 		onBack: function() {
 			sap.ui.getCore().byId("idApp").to("idView1");
 		},
-		onItemSelect: function(oEvent) {
-			var sPath = oEvent.getParameter("listItem").getBindingContextPath();
-			var indexSupp = sPath.split("/")[sPath.split("/").length - 1];
-			this.oRouter.navTo("detail2", {
-				suppId: indexSupp
-			});
-		},
-		onPress: function() {
-			sap.m.MessageBox.alert("Button was clicked");
-		},
-		onHover: function() {
-			sap.m.MessageBox.alert("Button was Hovered");
-		},
-		onCourseSelect: function(oEvent){
-			var country = this.getView().byId("country").getSelectedKey();
-			var courseName = this.getView().byId("course").getSelectedKey();
-			var allCourses = this.getView().getModel("local").getProperty("/courses");
-			if(country === "IN"){
-				for (var i = 0; i < allCourses.length; i++) {
-					if (allCourses[i].courseName === courseName) {
-						this.getView().getModel("local").setProperty("/template/fees",allCourses[i].fee);
-						this.getView().getModel("local").setProperty("/template/currency", "INR");
-						break;
-					}
-				}
 
-			}else{
-				for (var i = 0; i < allCourses.length; i++) {
-					if (allCourses[i].courseName === courseName) {
-						this.getView().getModel("local").setProperty("/template/fees",allCourses[i].usdFee);
-						this.getView().getModel("local").setProperty("/template/currency", "USD");
-						break;
-					}
-				}
-			}
-		},
-		herculis: function(oEvent) {
-			if(oEvent.getParameter("name") !== "newTemplate"){
-				return;
-			}
-
-			var oList = this.getView().byId("idRecent");
-			oList.bindAggregation("items", {
-				path: '/Templates',
-				template: new sap.m.DisplayListItem({
-					label: "{CourseName} - {Type} - {DemoDate}",
-					value: "{ClassTiming} / {NextClass}"
-				})
-			});
-			oList.attachUpdateFinished(this.counter);
-
-		},
-		counter: function(oEvent) {
-			var oList = oEvent.getSource();
-			var counts = oList.getItems().length;
-			oList.getHeaderToolbar().getContent()[0].setText("Today : " + counts);
-			var items = oList.mAggregations.items;
-			var value2;
-			var value1;
-			var id;
-			for (var i = 0; i < items.length; i++) {
-				value1 = items[i].mProperties.value.split("-")[0];
-				id = items[i].mProperties.value.split("-")[1];
-				if (this.getModel("local").getProperty("/AppUsers")[id]) {
-					value2 = this.getModel("local").getProperty("/AppUsers")[id].UserName;
-					oList.getItems()[i].setValue(value1 + " - " + value2);
-				}
-			}
-		},
-		supplierPopup: null,
-		oInp: null,
-		onPopupConfirm: function(oEvent) {
-			var selectedItem = oEvent.getParameter("selectedItem");
-			this.oInp.setValue(selectedItem.getLabel());
-		},
-
-		oSuppPopup: null,
-		onFilter: function() {
-
-			if (!this.oSuppPopup) {
-				this.oSuppPopup = new sap.ui.xmlfragment("oft.fiori.fragments.popup", this);
-
-				this.getView().addDependent(this.oSuppPopup);
-
-				this.oSuppPopup.setTitle("Suppliers");
-
-				this.oSuppPopup.bindAggregation("items", {
-					path: "/suppliers",
-					template: new sap.m.DisplayListItem({
-						label: "{name}",
-						value: "{city}"
-					})
-				});
-			}
-
-			this.oSuppPopup.open();
-		},
-
-		onRequest: function(oEvent) {
-
-			//Store the object of the input field on which F4 was press
-			this.oInp = oEvent.getSource();
-
-			//Step 1: Display a popup cl_gui_alv_grid, set_table_for_first_table
-			if (!this.supplierPopup) {
-				// this.supplierPopup = new sap.m.SelectDialog({
-				// 	title: "Supplier Popup",
-				// 	confirm: this.onPopupConfirm.bind(this)
-				// });
-				this.supplierPopup = new sap.ui.xmlfragment("oft.fiori.fragments.popup", this);
-
-				this.supplierPopup.setTitle("Cities");
-				//Will now supply the model set at the view level to its children
-				this.getView().addDependent(this.supplierPopup);
-
-				//this.supplierPopup.setTitle("")
-				//Step 2: Values to be populated with aggregation binding
-				this.supplierPopup.bindAggregation("items", {
-					path: "/cities",
-					template: new sap.m.DisplayListItem({
-						label: "{cityName}",
-						value: "{famousFor}"
-					})
-				});
-
-			}
-			//Step 3: Just open same popup once create
-			this.supplierPopup.open();
-
-		},
-		onConfirm: function(anubhav) {
-			//debugger;
-			if (anubhav === "OK") {
-				MessageToast.show("Your fruit has been approved successfully");
-				this.getView().byId("idApr").setVisible(false);
-			}
-		},
-		onSave: function(oEvent) {
-			var oLocal = oEvent;
-			console.log(this.getView().getModel("local").getProperty("/template"));
+		onTypeSelect : function(oEvent){
 			var that = this;
+			var type = oEvent.getSource().getSelectedKey();
+			var courseName = this.getView().byId("course").getSelectedKey();
+			var oFilter1 = new sap.ui.model.Filter("CourseName", "EQ", courseName);
+			var oFilter2 = new sap.ui.model.Filter("Type", "EQ", type);
+			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "GET", {
+					filters: [oFilter1,oFilter2]
+				}, {}, this)
+				.then(function(oData){
+					if(oData.results.length<1){
+						that.getView().byId('idSave').setEnabled(true);
+						that.getView().byId('idUpdate').setEnabled(false);
+					}
+					else{
+						that.getView().byId('idSave').setEnabled(false);
+						that.getView().byId('idUpdate').setEnabled(true);
+					}
+					that.getView().getModel('local').setProperty('/template',oData.results[0]);
+				}).catch(function(oError){
+					sap.m.MessageToast.show("template fetch failed");
+				});
+		},
+
+		onCourseSelect: function(oEvent){
+			var that = this;
+			var courseName = oEvent.getSource().getSelectedKey();
+			var type = this.getView().byId("type").getSelectedKey();
+			var oFilter1 = new sap.ui.model.Filter("CourseName", "EQ", courseName);
+			var oFilter2 = new sap.ui.model.Filter("Type", "EQ", type);
+			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "GET", {
+					filters: [oFilter1,oFilter2]
+				}, {}, this)
+				.then(function(oData){
+					if(oData.results.length<1){
+						that.getView().byId('idSave').setEnabled(true);
+						that.getView().byId('idUpdate').setEnabled(false);
+					}
+					else{
+						that.getView().byId('idSave').setEnabled(false);
+						that.getView().byId('idUpdate').setEnabled(true);
+					}
+					that.getView().getModel('local').setProperty('/template',oData.results[0]);
+				}).catch(function(oError){
+					sap.m.MessageToast.show("template fetch failed");
+				});
+		},
+		// counter: function(oEvent) {
+		// 	var oList = oEvent.getSource();
+		// 	var counts = oList.getItems().length;
+		// 	oList.getHeaderToolbar().getContent()[0].setText("Today : " + counts);
+		// 	var items = oList.mAggregations.items;
+		// 	var value2;
+		// 	var value1;
+		// 	var id;
+		// 	for (var i = 0; i < items.length; i++) {
+		// 		value1 = items[i].mProperties.value.split("-")[0];
+		// 		id = items[i].mProperties.value.split("-")[1];
+		// 		if (this.getModel("local").getProperty("/AppUsers")[id]) {
+		// 			value2 = this.getModel("local").getProperty("/AppUsers")[id].UserName;
+		// 			oList.getItems()[i].setValue(value1 + " - " + value2);
+		// 		}
+		// 	}
+		// },
+		onUpdate: function(oEvent) {
+			var oLocal = oEvent;
+			// console.log(this.getView().getModel("local").getProperty("/template"));
+			var that = this;
+			var templateData = this.getView().getModel("local").getProperty("/template");
+			templateData.DemoDate = this.getView().byId("inqDate").getDateValue();
+			templateData.CourseName = this.getView().byId("course").getSelectedKey();
+			templateData.Type = this.getView().byId("type").getSelectedKey();
+			var payload = {
+				"CourseName": templateData.CourseName,
+				"Type": templateData.Type,
+				"Template": templateData.Template,
+				"DemoDate": templateData.DemoDate,
+				"DemoInvite": templateData.DemoInvite,
+				"VideoLink": templateData.VideoLink,
+				"CoursePage": templateData.CoursePage,
+				"ClassTiming": templateData.ClassTiming,
+				"NextClass": templateData.NextClass,
+			};
+			var oFilter1 = new sap.ui.model.Filter("CourseName", "EQ", payload.CourseName);
+			var oFilter2 = new sap.ui.model.Filter("Type", "EQ", payload.Type);
 			that.getView().setBusy(true);
-			var leadData = this.getView().getModel("local").getProperty("/template");
-			var payload = leadData;
-			payload.DemoDate = this.getView().byId("inqDate").getDateValue();
-			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "POST", {},
-					payload, this)
+			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "PUT", { filters : [oFilter1,oFilter2]},
+					payload
+					, this)
 				.then(function(oData) {
 					that.getView().setBusy(false);
-					sap.m.MessageToast.show("Inquiry Saved successfully");
+					sap.m.MessageToast.show("template updated successfully");
 					that.destroyMessagePopover();
 					if (that.getView().byId("idRecent").getBinding("items")) {
 						that.getView().byId("idRecent").getBinding("items").refresh();
 					}
+				}).catch(function(oError){
+					that.getView().setBusy(false);
+					sap.m.MessageToast.show("template update failed");
 				});
 		},
-		onApprove: function() {
 
-			MessageBox.confirm("Do you want to approve this fruit", {
-				title: "confirmation",
-				//[this.functionName, this], as a substitute we use .bind(this) method
-				onClose: this.onConfirm.bind(this)
-			});
-
-		},
-		onGetNext: function(){
-			$.post('/MoveNextAc', {})
-				.done(function(data, status) {
-					sap.m.MessageBox.confirm(
-						"Bank Name    : " + data.BankName + "\n" +
-						"Account Name : " + data.accountName + "\n" +
-						"Account No   : " + data.accountNo + "\n" +
-						"IFSC Code    : " + data.ifsc + "\n" + "\n" +
-						"Note: Please send the screenshot of payment once done."
-					);
-				})
-				.fail(function(xhr, status, error) {
-					sap.m.MessageBox.error(xhr.responseText);
+		onSave: function(oEvent) {
+			var oLocal = oEvent;
+			// console.log(this.getView().getModel("local").getProperty("/template"));
+			var that = this;
+			var templateData = this.getView().getModel("local").getProperty("/template");
+			var payload = templateData;
+			payload.DemoDate = this.getView().byId("inqDate").getDateValue();
+			payload.CourseName = this.getView().byId("course").getSelectedKey();
+			payload.Type = this.getView().byId("type").getSelectedKey();
+			that.getView().setBusy(true);
+			this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Templates", "POST", {},
+					payload
+					, this)
+				.then(function(oData) {
+					that.getView().setBusy(false);
+					sap.m.MessageToast.show("template Saved successfully");
+					that.destroyMessagePopover();
+					if (that.getView().byId("idRecent").getBinding("items")) {
+						that.getView().byId("idRecent").getBinding("items").refresh();
+					}
+				}).catch(function(oError){
+					that.getView().setBusy(false);
+					sap.m.MessageToast.show("template Saving failed");
 				});
 		}
 		/**
